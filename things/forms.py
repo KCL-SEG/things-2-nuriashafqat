991d36d4910e18512d7687730fa43e15f33f4de1
nuriashafqat <nuriashafqat02@gmail.com>
2023-12-07 18:06:33 +0000

    Complete forms exercise

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
+from things.models import Thing
+from django import forms
 """Forms of the project."""
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
+
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
--- things/views.py
+++ things/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            print(form.errors)
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
